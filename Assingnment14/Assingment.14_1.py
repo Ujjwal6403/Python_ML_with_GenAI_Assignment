485ebf4bdbc38e1fd1e3f9a66fca6b84a7fb21f0
ujjwal6403 <ujjwalnarkhede2003@gmail.com>
2026-01-25 10:49:29 +0530

    square of No

--- Assingnment14/Assingment.14_1.py
+++ Assingnment14/Assingment.14_1.py
@@ -1,12 +1,12 @@
 #1>. Write a lambda function which accept one number and returns square of that number.
 
-
-add = lambda iNo : iNo * iNo
+ 
+square = lambda iNo : iNo * iNo
 
 def main():
    iNo = int(input("Enter the number : "))
   
-   Ret = add(iNo)
+   Ret = square(iNo)
    print("Square of no ",Ret)
    
 if __name__ == "__main__":
