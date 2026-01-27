a55734ed57dfedc6274d00f234f033ebe284f9b6
ujjwal6403 <ujjwalnarkhede2003@gmail.com>
2026-01-27 11:08:39 +0530

    Userdefine model create check prime number

--- Assingnment18/MarvellouseNum.py
+++ Assingnment18/MarvellouseNum.py
@@ -1,27 +1,20 @@
-def chkprime(Arr):
-   
-   for i in range(2,Arr):
-      if(Arr % i == 0):
-         break
-   if(Arr == i):
-      sum = sum + Arr
-      
-def main():
-   print("Enter the No of element : ")
-   Size = int(input())
-   
-   Data = list()
-   print("Enter the elements :")
-   
-   for i in range(Size):
-      value = int(input())
-      Data.append(value)
-   
-   print(Data)
-   
-   Ret = chkprime(Data)
-   
-   print("addition is prime number :  ",Ret)
+#3> write a program which accept N number from user and store it into list. 
+# return addition of all prime number form that list. 
+# main Python file accepts N numbers from user and pass 
+# each number to chkPrime() function which id part of our user defined module named as marvellousNum.
+# name of the function from main python file should be listPrime().
 
-if __name__=="__main__":
-   main()
+def chkprime(Arr):
+   sum = 0
+   print(Arr)
+   i = 2
+   for num in Arr:
+      for i in range(2,num+1):
+         if(num % i == 0):
+            break
+      if(num == i):
+         sum = sum + num
+        
+         
+   return sum     
+
--- Assingnment18/tempCodeRunnerFile.py
+++ Assingnment18/tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-Data
+#3> write a program which accept N number from user and store it into list. 
+# return addition of all prime number form that list. 
+# main Python file accepts N numbers from user and pass 
+# each number to chkPrime() function which id part of our user defined module named as marvellousNum.
+# name of the function from main python file should be listPrime().
